c57ee8d3a1a9adca227c7c0312d49ac505ac9be3
Raphael Stief <extern.stief_raphael@allianz.de>
2024-11-15 09:10:43 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,25 +4,33 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Raphael Stief',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
 		// What do you associate with the term 'CI/CD'?
 		associations: [
-			'Ehm, what?'
+			'Pipelines',
+			'GitHub Actions',
+			'Build/Test/Deploy steps',
+			'DevOps'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: [
+			'Jenkins',
+			'SonarQube'
+			],
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'How to actually use Jenkins',
+			'Learn more about the theory behind CI/CD'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'video games',
+			'running',
+			'music & concerts',
+			'doing my taxes?'
 		]
 	}
 });
